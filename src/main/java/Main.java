2ea2ef2f639357efe8932504907a2e8c0a9df4e8
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 14:48:22 -0400

    simplefyling the IF statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int temp = 32;
-        if(temp > 30) {
-            System.out.println("Its a hot day");
-            System.out.println("Drink water");
-        } else if (temp > 20)
-            System.out.println("Beaty of a day");
-        else
-            System.out.println("Cold day");
-
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+        System.out.println(hasHighIncome);
     }
 }
