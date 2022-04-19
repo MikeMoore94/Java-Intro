611e4392aa3e3f733121870e50f80943d6a6b788
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 14:12:09 -0400

    asked the user there age and printed in a string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        String result = percent.format(.01);
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
+         byte age = scanner.nextByte();
+        System.out.println("you are" + age);
     }
 }
