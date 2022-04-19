1f4e620c0cdf1838a14ec2801545155498960e0a
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 15:21:04 -0400

    used a for each loop to print ever item in a array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      String input = "";
-      while(true){
-          System.out.println("Input: ");
-          input = scanner.next().toLowerCase();
-          if (input.equals("pass"))
-              continue;
-          if (input.equals("quit"))
-              break;
-          System.out.println(input);
-      }
+      String[] fruits = {"apple", "orange", "mango"};
+
+      for (int i = 0; i < fruits.length; i++)
+          System.out.println(fruits[i]);
+      for (String fruit : fruits)
+          System.out.println(fruit);
 
     }
 }
