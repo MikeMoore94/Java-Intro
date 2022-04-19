90c8405b075531fd0e9f224837d3ae8f0dd409d6
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 15:01:34 -0400

    printed hello world five times

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-        System.out.println("Number: ");
-        int number = scanner.nextInt();
-
-        if (number % 5 == 0 && number % 3 == 0)
-            System.out.println("FizzBuzz");
-        else if (number % 3 == 0)
-            System.out.println("Buzz");
-        else if (number % 5 == 0)
-            System.out.println("Fizz");
-        else
-            System.out.println(number);
-    }
+      for(int i = 0; i < 5; i++){
+          System.out.println("Hello World");
+    }}
 }
