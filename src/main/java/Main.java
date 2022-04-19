73207e4016acbefc860ab2cb37cb5e0193241f12
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 17:11:50 -0400

    final morage calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,31 @@ public class Main {
 
 
     public static void main(String[] args) {
-      String[] fruits = {"apple", "orange", "mango"};
 
-      for (int i = 0; i < fruits.length; i++)
-          System.out.println(fruits[i]);
-      for (String fruit : fruits)
-          System.out.println(fruit);
+
+        final byte MONTHS_IN_YEARS = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner1.nextInt();
+
+        System.out.print("Annual Interest Rate (%): ");
+        float annualInterest = scanner1.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+
+        System.out.print("Period (Years): ");
+        int periodYear = scanner1.nextInt();
+        int periodMonth = periodYear*12;
+
+        double mortgage = principal*((monthlyInterest*(Math.pow(1+monthlyInterest, periodMonth)))/
+                (Math.pow(1+monthlyInterest, periodMonth)-1));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: "+mortgageFormatted);
+
+
 
     }
 }
