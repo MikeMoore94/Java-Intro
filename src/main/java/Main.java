b5268b0b2c38dfd005a38b8bc136d875788d75f5
Micheal Moore <mmoore@xpanxion.com>
2022-04-19 14:03:58 -0400

    printed a random number from 0-100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-      int result =  Math.round(1.1F);
+      double result = Math.round(Math.random() * 100) ;
         System.out.println(result);
     }
 }
