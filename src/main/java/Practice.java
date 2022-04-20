1fdd6ad9a2be2474aad68092283295df894c628a
Micheal Moore <mmoore@xpanxion.com>
2022-04-20 14:08:26 -0400

    added FizzBuzz to the practice.java class

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
-        System.out.println(x ==y);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 ==0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Buzz");
+        else if (number % 3 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
+
+
     }
 }
